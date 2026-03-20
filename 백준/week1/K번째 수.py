64d53e0ef21b7b24d2c1aac148ae923d85204738
gibeom0321 <rlqja0321@gmail.com>
2026-03-20 19:58:30 +0900

    [Silver V] Title: K번째 수, Time: 4848 ms, Memory: 723448 KB -BaekjoonHub

---
+++ 백준/week1/K번째 수.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+def merge_sort(unsorted_list):
+    if len(unsorted_list) <= 1:
+        return unsorted_list
+    mid = len(unsorted_list)//2
+    left = unsorted_list[:mid]
+    right = unsorted_list[mid:]
+
+    left_ = merge_sort(left)
+    right_ = merge_sort(right)
+    return merge(left_, right_)
+
+def merge(left, right):
+    i, j = 0, 0
+    sorted_list = []
+    while i<len(left) and j<len(right):
+        if left[i]<right[j]:
+            sorted_list.append(left[i])
+            i += 1
+        else:
+            sorted_list.append(right[j])
+            j += 1
+    while i<len(left):
+        sorted_list.append(left[i])
+        i += 1
+    while j<len(right):
+        sorted_list.append(right[j])
+        j += 1
+    return sorted_list
+data = merge_sort(data)
+print(data[k-1])
